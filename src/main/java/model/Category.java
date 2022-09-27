55cb3b619625e0ff26b8bb464b7390a4b3c3d2a4
keukjunior <mapsjunior@hotmail.com>
2022-09-27 16:50:06 +0000

    init10

---
+++ src/main/java/model/Category.java
@@ -0,0 +1,15 @@
+package model;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Category extends Abstrackentity{
+
+}
---
+++ src/main/java/model/Entreprise.java
@@ -0,0 +1,15 @@
+package model;
+
+import javax.persistence.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Entreprise extends Abstrackentity {
+
+}

89f2fe5b9fcf422805a8b853478b70e82f0a82bf
keukjunior <mapsjunior@hotmail.com>
2022-09-27 09:55:03 +0000

    init1

---
+++ src/test/java/keuk/com/StockbApplicationTests.java
@@ -0,0 +1,13 @@
+package keuk.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
